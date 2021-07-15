4ae7a9181340a880e78e7183134df907bf80f418
Korinfey <46531243+Korinfey@users.noreply.github.com>
2021-07-15 23:19:05 +0300

    1

---
+++ Foo Bar Recursion.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int result;
+
+int Num(int x, int y) {
+    y++;
+    if (y % 3 == 0)
+    {
+        printf("%dBar\n", y);
+    }
+    else if (y % 3 != 0)
+    {
+        printf("%dFoo\n", y);
+    }
+    if (y != x)
+    {
+        result = Num(x, y);
+        return result;
+    }
+
+}
+
+int main(void) {
+    int a, b = 0;
+    cin >> a;
+    Num(a, b);
+    return 0;
+}
